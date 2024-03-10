ea79b0332cf5892ccd09d20d4d9130b9fc1f42a6
Jack Turner <jack.turner123@icloud.com>
2024-03-10 22:46:14 +0000

    FInal

---
+++ InversityF1Strategy/OPENAI.swift
@@ -0,0 +1,8 @@
+//
+//  OPENAI.swift
+//  InversityF1Strategy
+//
+//  Created by Jack Turner on 07/03/2024.
+//
+
+import Foundation
---
+++ InversityF1Strategy/TeamRadio.swift
@@ -0,0 +1,8 @@
+//
+//  TeamRadio.swift
+//  InversityF1Strategy
+//
+//  Created by Jack Turner on 02/03/2024.
+//
+
+import Foundation
